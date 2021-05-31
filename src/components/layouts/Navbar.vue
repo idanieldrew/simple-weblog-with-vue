fcc33c4fb1d990ebaf12343b9aafaf6014cfde01
idanieldrew <danielgia35@gmail.com>
2021-05-31 18:17:28 +0430

    navbar complate

---
+++ src/components/layouts/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav id="navbar" class="bg-gray-300 sm:px-6 sm:py-4 shadow z-10">
+    <div class="flex sm:flex-row flex-col container">
+      <div class="font-bold text-2xl">
+        <span class="text-blue-600">Daniel</span>
+        <span class="text-red-600">Drew</span>
+      </div>
+      <div class="sm:px-12 sm:m-auto">
+        <Link to="/shop" name="درباره ما" />
+        <Link to="/about" name="اطلاعات" />
+        <Link to="/login" name="لاگین" />
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import Link from './NavbarLinks.vue'
+export default {
+      components:{
+            Link
+      }
+};
+</script>
---
+++ src/components/layouts/NavbarLinks.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-link class="sm:px-6 text-sm text-black hover:text-red-600" :to="to">{{ name }}</router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
